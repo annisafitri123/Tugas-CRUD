559d56a23924dc35c3588bbba8ea66da98b9ce13
Annisa fitri <39754308+annisafitri123@users.noreply.github.com>
2018-08-26 19:11:43 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+$host ="localhost";
+$user ="root";
+$pass ="";
+$db   ="tokobuku";
+
+$conn =mysqli_connect($host, $user, $pass,$db);
+	if ($conn==false) {
+		echo "koneksi server gagal";
+	}else{
+		
+	}
+
+?>	
